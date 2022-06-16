56a3ffffa34ab91e9951b82a14fc22e21cf2b8a7
Vika6329 <102681400+Vika6329@users.noreply.github.com>
2022-06-16 19:22:47 +0300

    finished project

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,35 @@
 $(function () {
 
+  let header__top = $("#header__top");
+  let header__content = $("#header__content");
+  let header__contentH = header__content.innerHeight();
+  let scrollPos = $(window).scrollTop();
+
+  $(window).on("scroll load resize", function () {
+    let header__contentH = header__content.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+    if (scrollPos > 100) {
+      header__top.addClass("fixed");
+    } else {
+      header__top.removeClass("fixed");
+    }
+
+    console.log(scrollPos);
+
+  });
+
+  $(".menu a, .header__content-link, .logo").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+
+
   $('.menu-btn, .header-menu__close').on('click', function () {
     $('.menu').toggleClass('menu--active');
     $('.menu-btn').toggleClass('menu-btn--active');
